6bc5b0ad1c9643d30129a3569c8756f1548f3ece
ericatsu <ekaatsu@st.ug.edu.gh>
2022-11-08 11:46:26 +0000

    schema created

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const {
 } = require('graphql')
 const app = express()
 
+//Schema defines the query section
+//Different sections of objects that can be queried
 const schema = new GraphQLSchema({
     query: new GraphQLObjectType({
         name: 'HelloWorld',
